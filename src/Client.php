1118258b5ccc62d3bcdc0ec823d47f2aa56aca4a
Thomas Kaltbeitzel <thomas@kaltbeitzel.me>
2025-01-05 12:09:37 +0100

    Use PSR-17 & PSR-18

--- src/Client.php
+++ src/Client.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Terminal42\WeblingApi;
 
-use Http\Client\HttpClient;
-use Http\Discovery\HttpClientDiscovery;
-use Http\Discovery\MessageFactoryDiscovery;
-use Http\Message\MessageFactory;
+use Exception;
+use JsonException;
+use Psr\Http\Client\ClientInterface as PsrClientInterface;
+use Http\Discovery\Psr18ClientDiscovery;
+use Http\Discovery\Psr17FactoryDiscovery;
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Terminal42\WeblingApi\Exception\ApiErrorException;
 use Terminal42\WeblingApi\Exception\HttpStatusException;
@@ -17,34 +20,42 @@ use Terminal42\WeblingApi\Exception\ParseException;
 class Client implements ClientInterface
 {
     /**
-     * @var HttpClient|null
+     * @var PsrClientInterface
      */
-    private $httpClient;
+    private PsrClientInterface $httpClient;
 
     /**
-     * @var MessageFactory
+     * @var RequestFactoryInterface
      */
-    private $requestFactory;
+    private RequestFactoryInterface $requestFactory;
 
     /**
      * @var string
      */
-    private $baseUri;
+    private string $baseUri;
 
     /**
      * @var array
      */
-    private $defaultQuery;
+    private array $defaultQuery;
 
-    public function __construct(string $subdomain, string $apiKey, int $apiVersion, HttpClient $httpClient = null, MessageFactory $messageFactory = null)
+    public function __construct(
+        string $subdomain,
+        string $apiKey,
+        int $apiVersion,
+        PsrClientInterface $httpClient = null,
+        RequestFactoryInterface $requestFactory = null)
     {
-        $this->httpClient = $httpClient ?: HttpClientDiscovery::find();
-        $this->requestFactory = $messageFactory ?: MessageFactoryDiscovery::find();
+        $this->httpClient = $httpClient ?: Psr18ClientDiscovery::find();
+        $this->requestFactory = $requestFactory ?: Psr17FactoryDiscovery::findRequestFactory();
 
         $this->baseUri = sprintf('https://%s.webling.ch/api/%s/', $subdomain, $apiVersion);
         $this->defaultQuery = ['apikey' => $apiKey];
     }
 
+    /**
+     * @throws HttpStatusException
+     */
     public function get(string $url, array $query = [])
     {
         try {
@@ -63,11 +74,18 @@ class Client implements ClientInterface
             }
 
             return $json;
-        } catch (\Exception $e) {
+        } catch (Exception $e)
+        {
+            throw new HttpStatusException($e->getMessage(), $e->getCode(), $e);
+        } catch (ClientExceptionInterface $e)
+        {
             throw new HttpStatusException($e->getMessage(), $e->getCode(), $e);
         }
     }
 
+    /**
+     * @throws HttpStatusException
+     */
     public function post(string $url, string $json)
     {
         try {
@@ -80,11 +98,18 @@ class Client implements ClientInterface
             }
 
             return $response->getBody()->getContents();
-        } catch (\Exception $e) {
+        } catch (Exception $e)
+        {
+            throw new HttpStatusException($e->getMessage(), $e->getCode(), $e);
+        } catch (ClientExceptionInterface $e)
+        {
             throw new HttpStatusException($e->getMessage(), $e->getCode(), $e);
         }
     }
 
+    /**
+     * @throws HttpStatusException
+     */
     public function put(string $url, string $json): void
     {
         try {
@@ -95,11 +120,18 @@ class Client implements ClientInterface
             if (204 !== $response->getStatusCode()) {
                 throw $this->convertResponseToException($response);
             }
-        } catch (\Exception $e) {
+        } catch (Exception $e)
+        {
+            throw new HttpStatusException($e->getMessage(), $e->getCode(), $e);
+        } catch (ClientExceptionInterface $e)
+        {
             throw new HttpStatusException($e->getMessage(), $e->getCode(), $e);
         }
     }
 
+    /**
+     * @throws HttpStatusException
+     */
     public function delete(string $url): void
     {
         try {
@@ -110,7 +142,11 @@ class Client implements ClientInterface
             if (204 !== $response->getStatusCode()) {
                 throw $this->convertResponseToException($response);
             }
-        } catch (\Exception $e) {
+        } catch (Exception $e)
+        {
+            throw new HttpStatusException($e->getMessage(), $e->getCode(), $e);
+        } catch (ClientExceptionInterface $e)
+        {
             throw new HttpStatusException($e->getMessage(), $e->getCode(), $e);
         }
     }
@@ -118,14 +154,18 @@ class Client implements ClientInterface
     /**
      * Convert a Guzzle Response to an exception. Useful if status code is not as expected.
      *
-     * @return \RuntimeException
+     * @param ResponseInterface $response
+     * @param Exception|null    $exception
+     *
+     * @return HttpStatusException|ApiErrorException|NotFoundException
+     * @throws JsonException
      */
-    protected function convertResponseToException(ResponseInterface $response, \Exception $exception = null)
+    protected function convertResponseToException(ResponseInterface $response, Exception $exception = null): HttpStatusException|ApiErrorException|NotFoundException
     {
-        $body = @json_decode((string) $response->getBody(), true);
+        $body = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
 
         if (!\is_array($body) || empty($body['error'])) {
-            return new HttpStatusException($response->getBody(), $response->getStatusCode(), $exception);
+            return new HttpStatusException($response->getBody()->getContents(), $response->getStatusCode(), $exception);
         }
 
         if (404 === $response->getStatusCode()) {
@@ -139,10 +179,11 @@ class Client implements ClientInterface
      * Builds an API request URI including authentication credentials.
      *
      * @param string $path
+     * @param array $query
      *
      * @return string
      */
-    private function buildUri($path, array $query = [])
+    private function buildUri(string $path, array $query = []): string
     {
         $query = array_merge($this->defaultQuery, $query);
 
